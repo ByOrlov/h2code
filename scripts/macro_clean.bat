eb0a53b9cef63868efb670990bbbecece0ba8dcb
Spec <spec@example.com>
2026-09-10 14:31:26 +0300

    init

--- scripts/macro_clean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Kill a lingering macro_run.exe and clear the Crystal cache entry for
-rem scripts/i18n_check.cr (fixes LNK1104: cannot open macro_run.exe).
-taskkill /f /im macro_run.exe
-for /d %%D in ("%LOCALAPPDATA%\crystal\cache\*i18n_check.cr") do rmdir /s /q "%%D"
